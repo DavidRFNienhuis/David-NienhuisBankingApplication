87103b219f7148e9263e012f3e918faedb1bef75
DavidRFNienhuis <84037660+DavidRFNienhuis@users.noreply.github.com>
2021-11-08 12:40:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ function Spa() {
     <script src="https://unpkg.com/react-router/umd/react-router.min.js" crossorigin></script>
     <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js" crossorigin></script>
 
-      <UserContext.Provider value={{users:[{name:'Dave',email:'Dave@Davescape.com',password:'secret',balance:1000}]}}>
+      
         <div className="container" style={{padding: "20px"}}>
+          <UserContext.Provider value={{users:[{name:'Dave',email:'Dave@Davescape.com',password:'secret',balance:1000}]}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
           <Route path="/login/" component={Login} />
           <Route path="/deposit/" component={Deposit} />
           <Route path="/withdraw/" component={Withdraw} />
           <Route path="/alldata/" component={AllData} />
+          </UserContext.Provider>
         </div>
-      </UserContext.Provider>      
+            
     </HashRouter>
   );
 }
